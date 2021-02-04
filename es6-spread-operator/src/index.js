d5bd54cab96577afb8cebff3a26bdb1f2a260a32
iamharshgoel <harshgoel0012@gmail.com>
2021-02-04 22:57:57 +0530

    index js file

---
+++ es6-spread-operator/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// const citrus = ["lime", "lemon", "orange"]
+// const fruits = ["apple", "banana", ...citrus]
+
+// console.log(fruits);
